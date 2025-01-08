902e0a080077fdf51186c40c134e56541951c4a9
Matheu5Leone <matheus.lsilva@sptech.school>
2025-01-08 15:50:22 -0300

    add metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Matheus Leone - Portfolio</title>
+        <link rel="icon" href="/portfolio/coroa.png" />
+        <meta property="og:title" content="Matheus Leone - Portfolio" />
+        <meta property="og:description" content="Learn more about me as a professional." />
+        <meta property="og:image" content="/portfolio/coroa.png" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  )
 }
