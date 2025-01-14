a71522e4147366ba091d9bf75ce151942c3be423
Matheu5Leone <matheus.lsilva@sptech.school>
2025-01-14 14:07:15 -0300

    add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const ANALYTICS = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
   return (
     <>
       <Head>
@@ -11,8 +13,22 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta property="og:title" content="Matheus Leone - Portfolio" />
         <meta property="og:description" content="Learn more about me as a professional." />
         <meta property="og:image" content="/portfolio/coroa.png" />
+
+        {/* Google Analytics */}
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${ANALYTICS}`}>
+        </Script>
+        <Script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${ANALYTICS}');
+            `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
-  )
+  );
 }
